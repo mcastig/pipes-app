ae98fdbbd610538973ddd0f7b5c044647629fb1f
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-16 20:55:24 -0600

    Add basic pipes such as lowercase, uppercasea and titlecase

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.component.html'
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+  nameLower = signal('Nemesis');
+  nameUpper = signal('NEMESIS');
+  fullName = signal('nEmEsIs NeMeSiS');
+}
