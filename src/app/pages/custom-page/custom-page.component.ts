73144455f12c2fc74a969b0dfa5fba5ae533a736
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-18 15:34:33 -0600

    Implement hero-color and hero-text-color pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../mock-data/hero.data';
 import { CanFlyPipe } from "../../pipes/can-fly.pipe";
+import { HeroColorPipe } from "../../pipes/hero-color.pipe";
+import { HeroTextColorPipe } from "../../pipes/hero-text-color.pipe";
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
