0e9a77f6da15906254010b8314f68895d0335e0f
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-18 15:11:10 -0600

    Implement custom pipe called can-fly.pipe.ts

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../mock-data/hero.data';
+import { CanFlyPipe } from "../../pipes/can-fly.pipe";
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
