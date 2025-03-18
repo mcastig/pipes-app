aeb8f3e1cd45b8560ba2897bb013717c62799d66
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-18 14:56:20 -0600

    Add the markup of the heroes table

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../mock-data/hero.data';
 
 @Component({
   selector: 'app-custom-page',
@@ -9,6 +10,7 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 export default class CustomPageComponent {
   public name = signal('Terry Silver');
   public upperCase = signal(true);
+  public heroes = signal(heroes);
 
   public toggleCase() {
     this.upperCase.update(value => !value);
