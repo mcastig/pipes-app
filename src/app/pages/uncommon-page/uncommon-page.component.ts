f3bcc9cbfbcb4b99ae7188aae7c0aa313ffdcadb
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-17 23:53:17 -0600

    Add example for using JsonPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
-import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe } from '@angular/common';
 
 const client1 = {
   name: 'Fernando',
@@ -24,6 +24,7 @@ const client2 = {
     I18nSelectPipe,
     I18nPluralPipe,
     SlicePipe,
+    JsonPipe,
   ],
   templateUrl: './uncommon-page.component.html',
 })
