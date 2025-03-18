3a14133a7c0efd43662a432368da27bf71023511
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-18 14:25:58 -0600

    Add an example of custom pipe called TogglePipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  public name = signal('Terry Silver');
+  public upperCase = signal(true);
+
+  public toggleCase() {
+    this.upperCase.update(value => !value);
+  }
+}
