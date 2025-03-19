6cf06e0ea962900d98d39b3f73ff4edf75edefe3
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-18 20:14:45 -0600

    Add HeroFilterPipe to order the list by search query

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -8,6 +8,7 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from "../../pipes/hero-creator.pipe";
 import { HeroSortByPipe } from "../../pipes/hero-sort-by.pipe";
 import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
@@ -18,7 +19,8 @@ import { Hero } from '../../interfaces/hero.interface';
     HeroTextColorPipe,
     TitleCasePipe,
     HeroCreatorPipe,
-    HeroSortByPipe
+    HeroSortByPipe,
+    HeroFilterPipe,
 ],
   templateUrl: './custom-page.component.html',
 })
@@ -27,6 +29,7 @@ export default class CustomPageComponent {
   public upperCase = signal(true);
   public heroes = signal(heroes);
   public sortBy = signal<keyof Hero | null>(null);
+  public searchQuery = signal('');
 
   public toggleCase() {
     this.upperCase.update(value => !value);
